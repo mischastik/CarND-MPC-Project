d475bc733beca66fa0ed37b8e3e2fb775ebca88f
mischastik <michael.balda@gmail.com>
2018-04-12 23:57:27 +0200

    WIP: Copied code from quizzes with some basic adaptions.

--- src/MPC.h
+++ src/MPC.h
@@ -6,6 +6,11 @@
 
 using namespace std;
 
+// Set the timestep length and duration
+size_t N = 25;
+double dt = 0.05;
+double ref_v = 40;
+
 class MPC {
  public:
   MPC();
@@ -14,7 +19,7 @@ class MPC {
 
   // Solve the model given an initial state and polynomial coefficients.
   // Return the first actuatotions.
-  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs);
+  vector<double> Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs, double latencySecs);
 };
 
 #endif /* MPC_H */
